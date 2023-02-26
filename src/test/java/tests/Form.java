686a7aa75896f2f2a0ce851bd024f9c1a7433843
yuriy.lvatov <lvatov.yuriy@rt.ru>
2023-02-26 16:28:53 +0300

    add code from foarm

--- src/test/java/tests/Form.java
+++ src/test/java/tests/Form.java
@@ -1,9 +1,20 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.awt.*;
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.value;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.files.DownloadActions.click;
+import static com.codeborne.selenide.impl.Html.text;
 
 public class Form {
 
@@ -16,13 +27,45 @@ public class Form {
     @Test
     void successfulTest() {
         open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
 
         $("[id=firstName]").setValue("Andrey");
+
         $("[id=lastName]").setValue("Frolov");
+
         $("[id=userEmail]").setValue("Frolov@mail.ru");
-        $("[id=gender-radio-3]").setValue("true");
+
+        $(byText("Male")).click();
+
         $("[id=userNumber]").setValue("9603456789");
-        $("[id=dateOfBirth]").click();
+
+        $("[id=dateOfBirthInput]").click();
+        $(By.className("react-datepicker__year-select")).selectOptionByValue("1991");
+        $(By.className("react-datepicker__month-select")).selectOptionByValue("4");
+        $(byText("18")).click();
+
+      //  $(By.className("subjects-auto-complete__control css-yk16xz-control")).sendKeys("Histor");
+      //  $(byText("History")).click();
+      //  $("[id=subjectsContainer]").setValue("Maths");
+      //  $(byText("Maths")).click();
+
+        $(byText("Sports")).click();
+        $(byText("Reading")).click();
+        $(byText("Music")).click();
+
+        $("[id=uploadPicture]").click();
+
+        $("[id=currentAddress]").setValue("Bad long history...");
+
+        $("[id=state]").click();
+        $(byText("Haryana")).click();
+
+        $("[id=city]").click();
+        $(byText("Karnal")).click();
+
+        $("[id=submit]").click();
+
+
 
     }
 
